555fa973739ba24f41deec81fcb2d553150d08ed
jano <janoiangonzalez@gmail.com>
2026-03-04 13:01:53 -0300

    Ejercicios 2,3,5,6 terminados, el 3 lo tengo pendiente

--- guias/guia1/guia1.hs
+++ guias/guia1/guia1.hs
@@ -1,12 +1,12 @@
--- Practica N 1 
+-- Practica N 1
 
-{- 
-Ejercicio 1 
+{-
+Ejercicio 1
 
 i)
 a)
 
-max2 :: Num a => (a,a) -> a 
+max2 :: Num a => (a,a) -> a
 max2 (x, y) | x >= y = x
             | otherwise = y
 
@@ -17,33 +17,33 @@ normaVectorial (x, y) = sqrt (x^2 + y^2)
 
 c)
 
-subtract :: Num a =>  a -> a -> a 
+subtract :: Num a =>  a -> a -> a
 subtract = flip (-)
 
 d)
 
 predecesor :: Num c => c -> c
-predecesor = subtract 1 
+predecesor = subtract 1
 
 e)
 
-evaluarEnCero :: Num a => (a -> b) -> b 
-evaluarEnCero = \f -> f 0 
+evaluarEnCero :: Num a => (a -> b) -> b
+evaluarEnCero = \f -> f 0
 
 f)
 
-dosVeces :: 
-dosVeces = \f -> f . f 
+dosVeces ::
+dosVeces = \f -> f . f
 
 f1 = (a1 -> b1)
 f2 = (a2 -> b2 )
-f . f = (a2 -> b2) -> b1 
+f . f = (a2 -> b2) -> b1
 
 (.) :: (b -> c) -> (a -> b) -> a -> c
 
 f1 = (b -> c)
 f2 = (a -> b)
-a = = b = c 
+a = = b = c
 
 Entonces:
 
@@ -76,7 +76,7 @@ flip2 :: (a2 -> b2 -> c2) -> (b2 -> a2 -> c2)
 
 { (a1 -> (b1 -> c1)) =? (a2 -> b2 -> c2) -> (b2 -> a2 -> c2))
 
-a1 = a2 -> b2 -> c2 , 
+a1 = a2 -> b2 -> c2 ,
 
 b1 -> c1 =? b2 -> (a2 -> c2)
 
@@ -101,3 +101,125 @@ normaVectorial :: Floating a => a -> a -> a
 normaVectorial x y = sqrt (x^2 + y^2)
 
 -}
+
+-- Ejercicicio 2
+
+-- i. Defnir la función curry, que dada una función de dos argumentos, devuelve su equivalente currificada
+{- HLINT ignore "Use sum" -}
+{- HLINT ignore "Use map" -}
+
+xcurry :: ((a, b) -> c) -> a -> b -> c
+xcurry f x y = f (x, y)
+
+-- ii
+xuncurry :: (a -> b -> c) -> (a, b) -> c
+xuncurry = uncurry
+
+-- iii  Me parece que no se podria dejar fijo los tipos.
+
+-- Ejercicio 3
+
+-- i
+sumfoldr :: (Num a) => [a] -> a
+sumfoldr = sum
+
+elemfoldr :: (Eq a) => [a] -> a -> Bool
+elemfoldr xs e = foldr (\x rec -> x == e || rec) False xs
+
+concatfoldr :: [a] -> [a] -> [a]
+concatfoldr xs ys = foldr (:) ys xs
+
+filterfoldr :: [a] -> (a -> Bool) -> [a]
+filterfoldr xs p = foldr (\x rec -> if p x then x : rec else rec) [] xs
+
+mapFoldr :: [a] -> (a -> b) -> [b]
+mapFoldr xs f = map f xs
+
+-- ii
+
+mejorSegun :: (a -> a -> Bool) -> [a] -> a
+mejorSegun criterio = foldr1 (\x rec -> if criterio x rec then x else rec)
+
+-- iii
+
+sumasParciales :: (Num a) => [a] -> [a]
+sumasParciales = foldr (\x rec -> x : map (x +) rec) []
+
+-- iv
+
+sumaAlt :: (Num a) => [a] -> a
+sumaAlt = foldr (-) 0
+
+-- v
+
+sumaAltv2 :: (Num a) => [a] -> a
+sumaAltv2 xs = fst (foldl f (0, 1) xs)
+  where
+    f (ac, signo) x = (ac + signo * x, -signo)
+
+-- Ejercicio 4
+
+-- Ejercicio 5
+
+{--
+elementosEnPosicionesPares no es recursion estructural porque accede a su subestructura en el condicional del if (null xs),
+por lo que no es estructural.
+
+entrelazar se hace sobre la lista (x:xs), por lo que si bien puede parecer al principio que tambien estamos accediendo a la
+subestructura de ella misma, enrealidad es sobre ys, por lo que esta si es con recursion estructural.
+
+-}
+
+entrelazar :: [a] -> [a] -> [a]
+entrelazar = foldr (\x rec ys -> if null ys then rec ys else x : head ys : rec (tail ys)) (const [])
+
+{--
+Ayudita para cuando hacemos recursion con dos estructuras a la ves:
+
+Mira la definicion de foldr
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr f z [] = z
+foldr f z (x : xs) = f x (foldr f z xs)
+
+a es siempre la cabeza de la lista que posta estamos recorriendo. OK
+
+b es el tipo de retorno, y este tipo es arbitrario, por lo que podes elegir el que vos quieras! Hasta una FUNCION
+
+Entonces porque b no toma la otra estructura? (ys).
+
+b queda ([a] -> resultado)
+                                                                                                  2nda est
+Entonces el tipo del foldr: foldr :: (a -> ([a] -> res) -> ([a] -> res)) -> ([a] -> res) -> [a] -> [a] -> res
+
+Estamos haciendo:
+
+recursion sobre xs
+pero el resultado es
+una funcion que espera ys
+
+o sea:
+
+entrelazar xs = funcion que espera ys
+
+--}
+
+-- Ejercicio 6
+
+recr :: (a -> [a] -> b -> b) -> b -> [a] -> b
+recr _ z [] = z
+recr f z (x : xs) = f x xs (recr f z xs)
+
+-- a 
+sacarUna :: (Eq a) => a -> [a] -> [a]
+sacarUna e = recr (\x xs rec -> if e == x then xs else x : rec) []
+-- b 
+{-- 
+Lo malo de usar recursion estructural (foldr) es que no tenemos acceso a la subestructura de la lista, por lo que eso 
+hace que no podamos obtener informacion en cada paso recursivo de la misma, por ende se suele utilizar recursion estructural
+cuando tenemos que hacer proyecciones sin recorrerla completamente. 
+--}
+
+-- c 
+insertarOrdenado :: (Ord a) => a -> [a] -> [a]
+insertarOrdenado e = recr (\x xs rec -> if e < x then e : x : xs else x : rec) [e]
